c4eee2acb8629edf457f10de9ba57a3fb8589c66
Jacob Boyles <fixterjake@gmail.com>
2021-04-02 18:54:02 -0400

    Update to add bug reports and staffing requests

---
+++ ZDC.Core/Controllers/BugReportsController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZDC.Core.Data;
+using ZDC.Models;
+
+namespace ZDC.Core.Controllers
+{
+    [Route("api/[controller]")]
+    public class BugReportsController : Controller
+    {
+        private readonly ZdcContext _context;
+
+        public BugReportsController(ZdcContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<BugReport>>> GetBugReports()
+        {
+            return Ok(await _context.BugReports.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BugReport>> GetBugReport(int id)
+        {
+            var report = await _context.BugReports.FindAsync(id);
+
+            if (report == null) return NotFound($"Bug report: {id} not found");
+
+            return Ok(report);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BugReport>> PutBugReport(int id, [FromBody] BugReport data)
+        {
+            var report = await _context.BugReports.FindAsync(id);
+
+            if (report == null) return NotFound($"Bug report: {id} not found");
+
+            _context.Entry(report).CurrentValues.SetValues(data);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(report);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BugReport>> PostBugReport([FromBody] BugReport data)
+        {
+            if (!ModelState.IsValid) return BadRequest(data);
+
+            await _context.BugReports.AddAsync(data);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BugReport>> DeleteBugReport(int id)
+        {
+            var report = await _context.BugReports.FindAsync(id);
+
+            if (report == null) return NotFound($"Bug report: {id} not found");
+
+            _context.BugReports.Remove(report);
+
+            return Ok(report);
+        }
+    }
+}
---
+++ ZDC.Core/Controllers/StaffingRequestsController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZDC.Core.Data;
+using ZDC.Models;
+
+namespace ZDC.Core.Controllers
+{
+    [Route("api/[controller]")]
+    public class StaffingRequestsController : Controller
+    {
+        private readonly ZdcContext _context;
+
+        public StaffingRequestsController(ZdcContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<StaffingRequest>>> GetStaffingRequests()
+        {
+            return Ok(await _context.StaffingRequests.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StaffingRequest>> GetStaffingRequest(int id)
+        {
+            var request = await _context.StaffingRequests.FindAsync(id);
+
+            if (request == null) return NotFound($"Staffing request: {id} not found");
+
+            return Ok(request);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<StaffingRequest>> PutStaffingRequest(int id, [FromBody] StaffingRequest data)
+        {
+            var request = await _context.StaffingRequests.FindAsync(id);
+
+            if (request == null) return NotFound($"Staffing request: {id} not found");
+
+            _context.Entry(request).CurrentValues.SetValues(data);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(request);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<StaffingRequest>> PostStaffingRequest([FromBody] StaffingRequest data)
+        {
+            if (!ModelState.IsValid) return BadRequest(data);
+
+            await _context.StaffingRequests.AddAsync(data);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<StaffingRequest>> DeleteStaffingRequest(int id)
+        {
+            var request = await _context.StaffingRequests.FindAsync(id);
+
+            if (request == null) return NotFound($"Staffing request: {id} not found");
+
+            _context.StaffingRequests.Remove(request);
+
+            return Ok(request);
+        }
+    }
+}
